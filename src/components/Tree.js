a5a14aa427ff61aeb351d967513bc9e45090cffc
YashwanthC <cyashwanthkumar@presidio.com>
2022-12-05 22:13:47 +0530

    Fixed refreshing tokens issue

--- src/components/Tree.js
+++ src/components/Tree.js
@@ -6,7 +6,7 @@ import jwt_decode from "jwt-decode";
 const DASHBOARD_UUID = "916325f0-aa8f-45d9-b6f9-b674a9d4a8bb";
 const SUPERSET_DOMAIN = "http://54.152.68.117:8080";
 
-function getAccessToken() {
+async function getAccessToken() {
   const login_url = "api/v1/security/login";
   const login_payload = {
     password: "Test@123",
@@ -14,17 +14,17 @@ function getAccessToken() {
     refresh: true,
     username: "admin",
   };
-  axios
-    .post(login_url, login_payload)
-    .then((res) => {
-      localStorage.setItem("access_token", res.data.access_token);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+  const response = await axios.post(login_url, login_payload);
+  // .then((res) => {
+  //   localStorage.setItem("access_token", res.data.access_token);
+  // })
+  // .catch((error) => {
+  //   console.log(error);
+  // });
+  localStorage.setItem("access_token", response.data.access_token);
 }
 
-function getGeustToken() {
+async function getGeustToken() {
   const guest_token_url = "api/v1/security/guest_token/";
 
   const guest_token_payload = {
@@ -48,7 +48,7 @@ function getGeustToken() {
     !access_token ||
     jwt_decode(localStorage.getItem("access_token")).exp * 1000 < Date.now()
   ) {
-    getAccessToken();
+    await getAccessToken();
   }
 
   const headers = {
@@ -58,53 +58,60 @@ function getGeustToken() {
     },
   };
 
-  axios
-    .post(guest_token_url, guest_token_payload, headers)
-    .then((response) => {
-      console.log(response);
-      localStorage.setItem("tree_guest_token", response.data.token);
-    })
-    .catch((err) => {
-      if (err.data.message === "Token Expired") {
-        getAccessToken();
-      }
-    });
+  const response = await axios.post(
+    guest_token_url,
+    guest_token_payload,
+    headers
+  );
+  // .then((response) => {
+  //   console.log(response);
+  //   localStorage.setItem("tree_guest_token", response.data.token);
+  // })
+  // .catch((err) => {
+  // if (err.data.message === "Token Expired") {
+  //   getAccessToken();
+  // }
+  // });
+  localStorage.setItem("tree_guest_token", response.data.token);
 }
 
 const Tree = () => {
-  useEffect(async () => {
-    if (
-      !localStorage.getItem("tree_guest_token") ||
-      jwt_decode(localStorage.getItem("tree_guest_token")).exp * 1000 <
-        Date.now()
-    ) {
-      await getGeustToken();
-      embedDashboard({
-        id: DASHBOARD_UUID, // given by the Superset embedding UI
-        supersetDomain: SUPERSET_DOMAIN,
-        mountPoint: document.getElementById("container"), // any html element that can contain an iframe
-        fetchGuestToken: () => localStorage.getItem("tree_guest_token"),
-        dashboardUiConfig: {
-          hideTitle: true,
-          show_filters: 0,
-        }, // dashboard UI config: hideTitle, hideTab, hideChartControls (optional)
-      }).then((response) => {
-        console.log("Response of embed fun...", response);
-      });
-    } else {
-      embedDashboard({
-        id: DASHBOARD_UUID, // given by the Superset embedding UI
-        supersetDomain: SUPERSET_DOMAIN,
-        mountPoint: document.getElementById("container"), // any html element that can contain an iframe
-        fetchGuestToken: () => localStorage.getItem("tree_guest_token"),
-        dashboardUiConfig: {
-          hideTitle: true,
-          show_filters: 0,
-        }, // dashboard UI config: hideTitle, hideTab, hideChartControls (optional)
-      }).then((response) => {
-        console.log("Response of embed fun", response);
-      });
+  useEffect(() => {
+    async function fetchTokens() {
+      if (
+        !localStorage.getItem("tree_guest_token") ||
+        jwt_decode(localStorage.getItem("tree_guest_token")).exp * 1000 <
+          Date.now()
+      ) {
+        await getGeustToken();
+        embedDashboard({
+          id: DASHBOARD_UUID, // given by the Superset embedding UI
+          supersetDomain: SUPERSET_DOMAIN,
+          mountPoint: document.getElementById("container"), // any html element that can contain an iframe
+          fetchGuestToken: () => localStorage.getItem("tree_guest_token"),
+          dashboardUiConfig: {
+            hideTitle: true,
+            show_filters: 0,
+          }, // dashboard UI config: hideTitle, hideTab, hideChartControls (optional)
+        }).then((response) => {
+          console.log("Response of embed fun...", response);
+        });
+      } else {
+        embedDashboard({
+          id: DASHBOARD_UUID, // given by the Superset embedding UI
+          supersetDomain: SUPERSET_DOMAIN,
+          mountPoint: document.getElementById("container"), // any html element that can contain an iframe
+          fetchGuestToken: () => localStorage.getItem("tree_guest_token"),
+          dashboardUiConfig: {
+            hideTitle: true,
+            show_filters: 0,
+          }, // dashboard UI config: hideTitle, hideTab, hideChartControls (optional)
+        }).then((response) => {
+          console.log("Response of embed fun", response);
+        });
+      }
     }
+    fetchTokens();
   }, []);
 };
 
